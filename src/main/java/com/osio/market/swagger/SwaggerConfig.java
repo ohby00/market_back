1a1b93296c1d56d4332af79feb8a01d657811e55
ohby00 <quddnr217@naver.com>
2024-04-20 21:07:20 +0900

    수정쓰

---
+++ src/main/java/com/osio/market/swagger/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.osio.market.swagger;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * Swagger 인증 헤더 설정
+ */
+// http://localhost:8080/swagger-ui.html/
+@Configuration
+@SecurityScheme(
+        name = HttpHeaders.AUTHORIZATION,
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+}
